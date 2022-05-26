d46a2b1a712518be5518ff97283c5bb648b106a4
CarolaOrbuch <101721194+CarolaOrbuch@users.noreply.github.com>
2022-05-26 00:33:37 -0300

    Initial commit

---
+++ Assets/Script/ValidarContra.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ValidarContra : MonoBehaviour
+{
+    string contraCorrecta;
+    string contraUsr;
+    public Text ingresoUsr;
+    public Text textoMensaje;
+    public GameObject popUpMensaje;
+    // Start is called before the first frame update
+    void Start()
+    {
+        contraCorrecta = "12345";
+        popUpMensaje.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void validarContrasenia()
+    {
+        contraUsr = ingresoUsr.text;
+        if (contraUsr == contraCorrecta)
+        {
+            popUpMensaje.SetActive(true);
+            textoMensaje.text = "Bienvenido";
+            Debug.Log("Bienvenido");
+        }
+        else
+        {
+            popUpMensaje.SetActive(true);
+            textoMensaje.text = "Contraseña Inconrrecta";
+            Debug.Log("Contraseña Incorrecta");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# TP-02-Unity-UI-Carola-Orbuch
+ Programa de ingreso y chequeo de contraseña con UI
